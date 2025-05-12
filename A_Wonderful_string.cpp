b4034a06668524e71311e85ad425f694ca91d1f7
JohnKumar2103122 <2103122@student.ruet.ac.bd>
2025-05-12 16:49:41 +0600

    Add files via upload

---
+++ A_Wonderful_string.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void test() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int l = 1;
+    int r = n;
+    vector<int> a(n);
+    for (int i = n - 2; i >= 0; i--) {
+        if (s[i] == '<') {
+            a[i + 1] = l;
+            l++;
+        }
+        if (s[i] == '>') {
+            a[i + 1] = r;
+            r--;
+        }
+    }
+    a[0] = l;
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        test();
+    }
+    
+    return 0;
+}
